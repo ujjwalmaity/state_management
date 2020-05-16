129dbf3605d536a25d801b0b3770ce40c8e59bce
ujjwal <ujjwalmaity98@gmail.com>
2020-05-16 15:54:39 +0530

    Initial project setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,57 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String data = 'Top Secret data';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(data),
+        ),
+        body: Level1(data),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  final String data;
+
+  Level1(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level2(data),
+    );
+  }
+}
+
+class Level2 extends StatelessWidget {
+  final String data;
+
+  Level2(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(),
+        Level3(data),
+      ],
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  final String data;
+
+  Level3(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(data);
   }
 }
